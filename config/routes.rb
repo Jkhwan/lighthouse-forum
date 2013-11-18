fb77debd5761f25af2a10a72475c834e8c2d8294
Jason Wan <jwan823@gmail.com>
2013-11-17 20:09:23 -0800

    Map root to show all posts page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ LighthouseForum::Application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   resources :posts
-
+  root to: 'posts#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
